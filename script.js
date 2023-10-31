c89a0959a043cd7e58c8a48a40fff676ad53c017
Matheus Botelho <mtsbotelhosantos@gmail.com>
2023-10-31 15:14:59 -0300

    alteracoes finais

--- script.js
+++ script.js
@@ -80,3 +80,39 @@ let slides = document.querySelectorAll('.slideshow');
 
 
 //-------------------------------------------------
+
+
+//MASCARA CNPJ
+
+document.getElementById('produto-1-cnpj').addEventListener('input', function (e) {
+  var x = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,3})(\d{0,3})(\d{0,4})(\d{0,2})/);
+  e.target.value = !x[2] ? x[1] : x[1] + '.' + x[2] + '.' + x[3] + '/' + x[4] + (x[5] ? '-' + x[5] : '');
+});
+
+//MASCARA TELEFONE
+
+function mascara(o,f){
+  v_obj=o
+  v_fun=f
+  setTimeout("execmascara()",1)
+}
+function execmascara(){
+  v_obj.value=v_fun(v_obj.value)
+}
+function mtel(v){
+  v=v.replace(/\D/g,""); //Remove tudo o que não é dígito
+  v=v.replace(/^(\d{2})(\d)/g,"($1) $2"); //Coloca parênteses em volta dos dois primeiros dígitos
+  v=v.replace(/(\d)(\d{4})$/,"$1-$2"); //Coloca hífen entre o quarto e o quinto dígitos
+  return v;
+}
+function id( el ){
+return document.getElementById( el );
+}
+window.onload = function(){
+id('telefone').onkeyup = function(){
+  mascara( this, mtel );
+}
+}
+
+
+//SCROLL SMOOTH
